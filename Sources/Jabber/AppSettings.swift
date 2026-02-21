15740548238f1434c192ae9d6ec6524784bfdecf
Roberto Selbach <rselbach@rselbach.com>
2026-02-21 18:03:02 -0500

    Remove unused model load timeout setting key

--- Sources/Jabber/AppSettings.swift
+++ Sources/Jabber/AppSettings.swift
@@ -6,7 +6,6 @@ enum AppSettingKey {
     static let outputMode = "outputMode"
     static let hotkeyDisplay = "hotkeyDisplay"
     static let vocabularyPrompt = "vocabularyPrompt"
-    static let modelLoadTimeoutOverride = "modelLoadTimeoutOverride"
 }
 
 enum AppSettings {
